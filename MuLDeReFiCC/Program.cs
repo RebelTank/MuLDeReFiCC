bd43c5204ec1d2be97e4afce1434b3cc48afe81b
Steven Curtis <rebeltank@gmail.com>
2016-03-12 13:39:08 -0800

    Initialization
    Starting the long road to an awesome app!

---
+++ MuLDeReFiCC/Program.cs
@@ -0,0 +1,29 @@
+﻿/**
+* Copyright Steven Curtis 2016
+* MuLDeReFiCC: Music Library Deduplication, Reorganization, FIle Conversion, and Curation.
+* An application to gather your music libraries all into one place and clean them up, removing duplicates,
+* converting file types you never use, filling in metadata, and trying to place it all together in a way that makes sense.
+**/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuLDeReFiCC
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
